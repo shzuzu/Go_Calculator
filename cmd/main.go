a63eaae217f7183e33754296d41169b86c938431
sherm <shermatovzufar@gmail.com>
2024-12-19 23:19:58 +0300

    different commands in main are called by different commands

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,40 @@
 package main
 
-import "github.com/shzuzu/Go_Calculator/internal/application"
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/shzuzu/Go_Calculator/internal/application"
+)
 
 func main() {
+	mode := flag.String("mode", "console", "Application operating mode: console or server")
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Println("Please choose the mode, use --mode=console or --mode=server")
+		os.Exit(1)
+	}
+
 	app := application.New()
-	// app.Run()
-	app.RunServer()
+
+	switch *mode {
+	case "":
+		fmt.Println("Please choose the mode, use --mode=console or --mode=server")
+		os.Exit(1)
+	case "console":
+		fmt.Println("Starting calculator in console mode...")
+		app.Run()
+	case "server":
+		fmt.Println("Starting HTTP-server...")
+		err := app.RunServer()
+		if err != nil {
+			fmt.Println("Error via starting the server:", err)
+			os.Exit(1)
+		}
+	default:
+		fmt.Println("Unknown mode. Use --mode=console or --mode=server")
+		os.Exit(1)
+	}
 }
